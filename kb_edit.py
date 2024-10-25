41ac7bb0248666c44400e03e3f2019114e146b85
Claudio Cicali <claudio.cicali@gmail.com>
2024-10-25 19:53:18 +0200

    Something something

--- kb_edit.py
+++ kb_edit.py
@@ -40,45 +40,6 @@ class ImageViewerApp(QWidget):
         self.drawing = False
         self.mode = "edit"
 
-        # Animation parameters
-        # self.duration = 5000  # Animation duration in ms
-        # self.fps = 30  # Frames per second
-        # self.total_frames = int(self.duration / (1000 / self.fps))
-        # self.current_frame = 0
-        # self.timer = QTimer()
-        # self.timer.timeout.connect(self.update_frame)
-
-    # def update_frame(self):
-    #     if self.current_frame >= self.total_frames:
-    #         self.timer.stop()
-    #         return
-
-    #     rect_start = QRect(self.boxes["start"].begin, self.boxes["start"].end)
-    #     rect_end = QRect(self.boxes["end"].begin, self.boxes["end"].end)
-
-    #     # Interpolate the rectangle position
-    #     t = self.current_frame / self.total_frames
-    #     interpolated_rect = self.interpolate_rect(rect_start, rect_end, t)
-
-    #     # Crop the original image and resize it to fit the QLabel
-    #     cropped_pixmap = self.original_scaled_pixmap.copy(interpolated_rect)
-    #     scaled_pixmap = cropped_pixmap.scaled(
-    #         self.image_label.size(), aspectRatioMode=True
-    #     )
-
-    #     # Update the QLabel with the new frame
-    #     self.image_label.setPixmap(scaled_pixmap)
-
-    #     self.current_frame += 1
-
-    # def interpolate_rect(self, start_rect, end_rect, t):
-    #     """Interpolate between start and end rectangles based on t (0 to 1)."""
-    #     x = start_rect.x() + t * (end_rect.x() - start_rect.x())
-    #     y = start_rect.y() + t * (end_rect.y() - start_rect.y())
-    #     width = start_rect.width() + t * (end_rect.width() - start_rect.width())
-    #     height = start_rect.height() + t * (end_rect.height() - start_rect.height())
-    #     return QRect(int(x), int(y), int(width), int(height))
-
     def __setattr__(self, key, value):
         if key == "current_box" and value not in self.boxes.keys():
             raise ValueError(f"Invalid value for current_box: {value}")
@@ -101,7 +62,8 @@ class ImageViewerApp(QWidget):
         w = int(1920 / 2)
         h = int(w * 9 / 16)
         self.image_label.setFixedSize(w, h)
-        self.image_label.setStyleSheet("border: 2px solid silver")
+        # If you set a border, the border is part of the label!
+        self.image_label.setStyleSheet("background-color: black;")
 
         btn_layout = QHBoxLayout()
         btn_layout.addWidget(self.select_button)
@@ -111,7 +73,7 @@ class ImageViewerApp(QWidget):
         main_layout.addLayout(btn_layout)
 
         self.setLayout(main_layout)
-        self.setWindowTitle("PyQt5 Image Viewer with Rectangle Drawing")
+        self.setWindowTitle("Ken Burns Effect tools")
         self.setGeometry(300, 300, 500, 400)
         self.show()
 
@@ -120,38 +82,8 @@ class ImageViewerApp(QWidget):
             self.close()
         if event.key() == Qt.Key_R:
             self.handle_reset_parameters()
-        # if event.key() == Qt.Key_P:
-        #     self.handle_preview()
         if event.key() == Qt.Key_Escape:
             print("Esc pressed")
-
-    # def resizeEvent(self, event):
-    #     """Override the resize event to enforce a 16:9 aspect ratio (1080p ratio)."""
-    #     new_width = self.width()
-    #     new_height = int(new_width * 9 / 16)  # Enforce the 16:9 ratio
-
-    #     # If the calculated height is too large for the current window height, adjust the width
-    #     if new_height > self.height():
-    #         new_height = self.height()
-    #         new_width = int(new_height * 16 / 9)
-
-    #     # Resize the QLabel to maintain the aspect ratio
-    #     self.image_label.setFixedSize(new_width, new_height)
-
-    #     super().resizeEvent(event)  # Call the base class resize event
-
-    # def handle_preview(self):
-    #     if self.boxes["start"].begin is None or self.boxes["end"].begin is None:
-    #         return
-
-    #     if self.timer.isActive() or self.mode == "preview":
-    #         self.timer.stop()
-    #         self.update_image()
-    #         self.mode = "edit"
-    #     else:
-    #         self.mode = "preview"
-    #         self.current_frame = 0
-    #         self.timer.start(int(1000 / self.fps))
 
     def handle_open_image(self):
         file_name, _ = QFileDialog.getOpenFileName(
@@ -197,7 +129,7 @@ class ImageViewerApp(QWidget):
         if event.button() != Qt.LeftButton:
             return
 
-        pos = self.scaleMousePos(event)
+        pos = self.mapPosToPixmap(event)
         if pos is None:
             return
 
@@ -211,13 +143,13 @@ class ImageViewerApp(QWidget):
         if not self.drawing:
             return
 
-        pos = self.scaleMousePos(event)
+        pos = self.mapPosToPixmap(event)
         if pos is None:
             return
 
         box = self.boxes[self.current_box]
 
-        box.end = pos
+        (box.begin, box.end) = self.enforce_16_9_ratio(box.begin, pos)
 
         self.update_image()
 
@@ -236,33 +168,50 @@ class ImageViewerApp(QWidget):
 
         rect = QRect(box.begin, box.end).normalized()
 
-        remapped_rect = QRect(
-            rect.x() * self.image_scale_factor,
-            rect.y() * self.image_scale_factor,
-            rect.width() * self.image_scale_factor,
-            rect.height() * self.image_scale_factor,
-        )
-
         print(
             f"Current box: {self.current_box}, "
-            f"Rectangle drawn: Top-left ({rect.topLeft().x()}, {rect.topLeft().y()}), "
-            f"Bottom-right ({rect.bottomRight().x()}, {rect.bottomRight().y()})\n"
+            f"Rectangle drawn: ({rect.topLeft().x()}, {rect.topLeft().y()}), "
+            f"                 ({rect.bottomRight().x()}, {rect.bottomRight().y()})\n"
+            f"X, Y, W, H ({rect.topLeft().x()}, {rect.topLeft().y()}, {rect.width()}, {rect.height()})\n"
         )
 
-        print(
-            "Remapped"
-            f"Rectangle drawn: Top-left ({remapped_rect.topLeft().x()}, {remapped_rect.topLeft().y()}), "
-            f"Bottom-right ({remapped_rect.bottomRight().x()}, {remapped_rect.bottomRight().y()})\n\n"
-        )
+        if self.current_box == "end":
+            rect_start = self.mapRectToImage(
+                QRect(self.boxes["start"].begin, self.boxes["start"].end)
+            ).normalized()
+            rect_end = self.mapRectToImage(
+                QRect(self.boxes["end"].begin, self.boxes["end"].end)
+            ).normalized()
 
-        if self.current_box == "start":
+            x1 = rect_start.x()
+            x2 = rect_end.x()
+            y1 = rect_start.y()
+            y2 = rect_end.y()
+            w1 = rect_start.width()
+            w2 = rect_end.width()
+
+            print(
+                f"ffmpeg -i picture.jpg -vf \"zoompan=z='1+(({w2}/{w1})-1)*(on/duration)':x='{x1}+({x2}-{x1})*(on/duration)':y='{y1}+({y2}-{y1})*(on/duration)':d=300:s=1920x1080\" -t 10 output.mp4"
+            )
+        else:
             self.current_box = "end"
 
-    def scaleMousePos(self, event):
+    def mapRectToImage(self, rect):
+        return QRect(
+            int(rect.x() * self.image_scale_factor),
+            int(rect.y() * self.image_scale_factor),
+            int(rect.width() * self.image_scale_factor),
+            int(rect.height() * self.image_scale_factor),
+        )
+
+    def mapPosToPixmap(self, event):
+        if not self.image_label.geometry().contains(event.pos()):
+            return None
+
         label_pos = event.pos() - self.image_label.pos()
         # print(f"Label position: {label_pos}, Event position: {event.pos()}")
 
-        pixmap_size = self.original_pixmap_size
+        pixmap_size = self.original_scaled_pixmap.size()
         label_size = self.image_label.size()
 
         offset_x = (label_size.width() - pixmap_size.width()) / 2
@@ -271,10 +220,41 @@ class ImageViewerApp(QWidget):
         x = label_pos.x() - offset_x
         y = label_pos.y() - offset_y
 
-        if 0 <= x <= pixmap_size.width() and 0 <= y <= pixmap_size.height():
-            return QPoint(int(x), int(y))
+        # Not sure why we need this "-1 pixel" but it looks good
+        x = min(max(0, x), pixmap_size.width() - 1)
+        y = min(max(0, y), pixmap_size.height())
+
+        return QPoint(int(x), int(y))
+
+    def enforce_16_9_ratio(self, top_left, bottom_right):
+        # Calculate the current width and height of the rectangle
+        current_width = abs(bottom_right.x() - top_left.x())
+        current_height = abs(bottom_right.y() - top_left.y())
+
+        # Calculate the desired height for a 16:9 ratio, given the current width
+        desired_height = current_width * 9 / 16
+
+        # Calculate the desired width for a 16:9 ratio, given the current height
+        desired_width = current_height * 16 / 9
+
+        # Create copies of the original QPoint objects to avoid mutating the input
+        new_bottom_right = QPoint(bottom_right)
+
+        # Determine whether to adjust width or height
+        if current_height > desired_height:
+            # Adjust the height to maintain 16:9 ratio
+            if new_bottom_right.y() > top_left.y():
+                new_bottom_right.setY(int(top_left.y() + desired_height))
+            else:
+                new_bottom_right.setY(int(top_left.y() - desired_height))
         else:
-            return None
+            # Adjust the width to maintain 16:9 ratio
+            if new_bottom_right.x() > top_left.x():
+                new_bottom_right.setX(int(top_left.x() + desired_width))
+            else:
+                new_bottom_right.setX(int(top_left.x() - desired_width))
+
+        return top_left, new_bottom_right
 
     def update_image(self):
         if not self.original_scaled_pixmap:
@@ -288,6 +268,7 @@ class ImageViewerApp(QWidget):
             self.draw_box(painter, "start", True)
 
         painter.end()
+
         self.image_label.setPixmap(pixmap)
 
     def draw_box(self, painter, box_name, solid=False):
